fb0ac764e42142cef48d5a4d9144b7904a0b20c5
Vitaliy Derda <derdavitaliy@gmail.com>
2020-07-13 14:04:56 +0300

    added style for button go-back

--- src/view/MovieDetailsPage.js
+++ src/view/MovieDetailsPage.js
@@ -64,7 +64,7 @@ export default class MovieDetailsPage extends Component {
 					{!loading && movie && (
 						<>
 							<div className={styles.buttonWrapper}>
-								<button type="button" onClick={this.handleGoBack}>
+								<button className={styles.goBackBtn} type="button" onClick={this.handleGoBack}>
 									Go Back
 								</button>
 							</div>
